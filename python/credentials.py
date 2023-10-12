be13d05ceb214499572102e03d49eda8f10b0fc3
christiangjp <christiangjp@gmail.com>
2023-10-12 11:18:44 +0200

    Initial commit

---
+++ python/credentials.py
@@ -0,0 +1,11 @@
+# Snowflake credentials
+snowflake_user = 'SNOWFLAKE_USERNAME'
+snowflake_password = 'SNOWFLAKE_PASSWORD'
+snowflake_account = 'SNOWFLAKE_ACCOUNT'
+
+# Tableau credentials
+tableau_server =  'TABLEAU_SERVER_URL'
+tableau_token_name = 'TOKEN_NAME'
+tableau_token_secret = 'TOKEN_SECRET'
+tableau_site_name = '' # Tableau site name -- Optional
+tableau_site_url = '' # Tableau site URL -- Optional
